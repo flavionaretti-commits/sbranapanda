8bea02490de5e90b25add34e2a82d7a0cde2bb05
Shankh <flavio.naretti@gmail.com>
2026-09-13 00:06:56 +0200

    Corregge installazione e isolamento PWA

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
 const CACHE = 'sbranapanda-v8';
+const CACHE_PREFIX = 'sbranapanda-';
 const APP_SHELL = [
   './',
   './index.html',
@@ -24,7 +25,7 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys()
-      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(keys => Promise.all(keys.filter(key => key.startsWith(CACHE_PREFIX) && key !== CACHE).map(key => caches.delete(key))))
       .then(() => self.clients.claim())
   );
 });
